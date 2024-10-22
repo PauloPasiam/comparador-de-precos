1952fee23ce6f5f6aa642968123040d3f37bdc64
Paulo Pasiam <paulo.pasiam@gmail.com>
2024-10-22 18:51:46 -0300

    js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const searchForm =  document.querySelector('.search-form')
+const productList = document.querySelector('.product-list')
+
+searchForm.addEventListener('submit', async function(event){
+    event.preventDefault()
+    const inputValue = event.target[0].value 
+    const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${inputValue}`)
+    const products = (await data.json()).results.slice(0, 10)
+    
+    displayItems(products)
+
+})
+function displayItems(products){
+    productList.innerHTML = products.map( product => `
+        <div class="product-card">
+            <img src="${product.thumbnail}" alt="${product.title}">
+            <h3>${product.title}</h3>
+            <p class="product-price">${product.price.toLocaleString('pt-br', {style: "currency", currency: "BRL"})}</p>
+            <p class="product-store">Loja: ${product.seller.nickname}</p>
+        </div>
+        
+        `,
+    ).join('')
+}
